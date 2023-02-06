95f02acd029dbba1a3809730c69a476b8e3aa816
duktartamang <duktar13@gmail.com>
2023-02-06 17:03:00 +0545

    create a UI for features to add contact

--- src/main/java/com/smart/smartcontact/controller/UserController.java
+++ src/main/java/com/smart/smartcontact/controller/UserController.java
@@ -3,9 +3,12 @@ package com.smart.smartcontact.controller;
 
 import com.smart.smartcontact.dao.UserRepository;
 import com.smart.smartcontact.entities.User;
+import com.smart.smartcontact.entities.contact;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -16,15 +19,29 @@ public class UserController {
 
     @Autowired
     UserRepository userRepository;
-    @RequestMapping("/index")
-    public String dashboard(Model model, Principal principal){
+
+    @ModelAttribute
+    public void addCommonData(Model model,Principal principal){
         String userName = principal.getName();
 //        get user deatails using username
         User user = userRepository.getUserByUserName(userName);
 
         model.addAttribute("user",user);
+    }
 
-
+//    home dashboard
+    @RequestMapping("/index")
+    public String dashboard(Model model, Principal principal){
+        model.addAttribute("title","user Dashboard");
         return "normal/user_dashboard";
+    }
+//    open add form handler
+
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model)
+    {
+        model.addAttribute("title","Add Contact");
+        model.addAttribute("contact",new contact());
+        return "normal/add_contact_form";
     }
 }
